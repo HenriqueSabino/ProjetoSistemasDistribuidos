2830fb43dfdfd87a3673c236a4455e36e102553e
HenriqueSabino <h.sabinocosta@gmail.com>
2022-10-03 22:14:18 -0300

    Adding image storage to network

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,3 +3,4 @@
 docker compose -f SistemaDeEnvios/docker-compose.yaml up -d --build
 docker compose -f inventory/docker-compose.yaml up -d --build
 docker compose -f digital-wallet/docker-compose.yaml up -d --build
+docker compose -f image-storage/docker-compose.yaml up -d --build
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,3 +3,4 @@
 docker compose -f SistemaDeEnvios/docker-compose.yaml down -v
 docker compose -f inventory/docker-compose.yaml down -v
 docker compose -f digital-wallet/docker-compose.yaml down -v
+docker compose -f image-storage/docker-compose.yaml down -v
